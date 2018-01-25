fae68969c583b8f5336ac9b3c1c136eb8451fb5b
Marina Nieblas <marina.nieblas@hotmail.com>
2018-01-25 14:22:45 +0100

    x

--- storage/framework/classes.php
+++ storage/framework/classes.php
@@ -41,4 +41,5 @@
   'RainLab\\Translate\\Models\\Message' => 'plugins/rainlab/translate/models/Message.php',
   'RainLab\\Pages\\Classes\\MenuItem' => 'plugins/rainlab/pages/classes/MenuItem.php',
   'RainLab\\Pages\\Classes\\MenuItemReference' => 'plugins/rainlab/pages/classes/MenuItemReference.php',
+  'RainLab\\Pages\\FormWidgets\\MenuItems' => 'plugins/rainlab/pages/formwidgets/MenuItems.php',
 );
