9e5b291398deb9f1e67cc1646e5709d4af62ad4e
Jekyll <jekyll@Macbook-Andres.local>
2018-01-29 18:13:35 +0100

    3

--- plugins/rainlab/pages/components/StaticApp.php
+++ plugins/rainlab/pages/components/StaticApp.php
@@ -394,7 +394,7 @@ class StaticApp extends ComponentBase
 
         $data = array('experience' => 'experience');
 
-        $pdf = PDFS::loadView('pdf.experience', $data);
+        $pdf = PDFS::renderView('pdf.experience.html.twig', $data);
         $pdf_data = $pdf->output();
        
         $experience = 'experiencia';
