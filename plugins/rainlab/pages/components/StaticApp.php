f6a722f1c352fffd77dcd711b89506102e016c69
Jekyll <jekyll@Macbook-Andres.local>
2018-01-29 20:41:50 +0100

    4

--- plugins/rainlab/pages/components/StaticApp.php
+++ plugins/rainlab/pages/components/StaticApp.php
@@ -417,7 +417,7 @@ class StaticApp extends ComponentBase
 
         $data = array('experiencemarkup' => $experiencemarkup );
        
-        $pdf = PDFS::loadView('pdf.experience', compat('data'))->setOption('page-size', 'A4')->setOption('dpi',300);
+        $pdf = PDFS::loadView('pdf.experience', compact('data'))->setOption('page-size', 'A4')->setOption('dpi',300);
         $pdf_data = $pdf->output();
        
         $experience = 'experiencia';
