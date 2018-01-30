ce3f7dad3f280da26d30a1122d8e517f007fb55d
Jekyll <jekyll@Macbook-Andres.local>
2018-01-30 13:22:55 +0100

    7

--- plugins/rainlab/pages/components/StaticApp.php
+++ plugins/rainlab/pages/components/StaticApp.php
@@ -461,7 +461,7 @@ class StaticApp extends ComponentBase
         //
         
         $pdfd = new PDFS;
-        $pdf_data = $pdfd::generateFromHtml('<h1>Bill</h1><p>You owe me money, dude.</p>', '/tmp/bill-123.pdf');
+        $pdf_data = $pdfd::generateFromHtml('<h1>Bill</h1><p>You owe me money, dude.</p>', '/tmp/bill-12356.pdf');
         
         //$pdf_data = PDFS::getOutput('http://cadizturismo.jekyllme.com/es/experiencias/cinco');
         
