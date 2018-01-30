1c40d5c93ba67fd2cdee4b9488e73a602f763ef2
Jekyll <jekyll@Macbook-Andres.local>
2018-01-30 14:24:16 +0100

    8

--- plugins/rainlab/pages/components/StaticApp.php
+++ plugins/rainlab/pages/components/StaticApp.php
@@ -430,8 +430,10 @@ class StaticApp extends ComponentBase
         $preexperiencemarkup = $baseHtml.$experience[0]['markup'];
         
         $preexperiencemarkup2 = str_replace('.svg', '.png', $preexperiencemarkup);
+
+         $preexperiencemarkup3 = str_replace('</p>', '</p><br/><br/>', $preexperiencemarkup2);
         
-        $experiencemarkup = str_replace('/storage/', 'http://cadizturismo.jekyllme.com/storage/', $preexperiencemarkup2);
+        $experiencemarkup = str_replace('/storage/', 'http://cadizturismo.jekyllme.com/storage/', $preexperiencemarkup3);
 
         //dd(1);
 
