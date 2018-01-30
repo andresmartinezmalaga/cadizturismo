0566251adbba5561d75f589f62ed11994208b20b
Jekyll <jekyll@Macbook-Andres.local>
2018-01-30 12:02:21 +0100

    22

--- plugins/rainlab/pages/components/StaticApp.php
+++ plugins/rainlab/pages/components/StaticApp.php
@@ -458,7 +458,7 @@ class StaticApp extends ComponentBase
                
         //$renderedHtml = Twig::parse($template);
         
-        $pdf_data = PDFS::getOutput('http://cadizturismo.jekyllme.com/es/experiencias/una');
+        $pdf_data = PDFS::getOutput('http://cadizturismo.jekyllme.com/es/experiencias/cinco');
         
         $experience = 'experiencia';
 
