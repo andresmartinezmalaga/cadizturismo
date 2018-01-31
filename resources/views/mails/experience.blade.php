e3931b4b86a685bbb16c0f4008944eaa327136f8
Jekyll <jekyll@Macbook-Andres.local>
2018-01-31 23:07:06 +0100

    1

--- resources/views/mails/experience.blade.php
+++ resources/views/mails/experience.blade.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Email Framework - HTML Email Fluid Grid Layout</title>
+
     <style type="text/css">
 
 
@@ -124,9 +118,8 @@
         }
 
     </style>
-</head>
-<body>
-<center>
+
+
     <div class="all-element">
         <div style="max-width: 640px; margin: auto;">
             <!--[if mso]>
@@ -232,8 +225,3 @@
             <![endif]-->
         </div>
     </div>
-
-
-</center>
-</body>
-</html>
