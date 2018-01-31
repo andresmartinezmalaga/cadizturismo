1ed32a84c2f0061ac09756e217c47de57c94713a
Marina Nieblas <marina.nieblas@hotmail.com>
2018-01-31 13:57:34 +0100

    x

--- plugins/rainlab/pages/classes/Experience.php
+++ plugins/rainlab/pages/classes/Experience.php
@@ -165,7 +165,7 @@ class Experience extends ContentBase
           </div>
 
           <div class="txt-container">
-            <p class="title">TITULAR 1</p>
+            <h3 class="title">TITULAR 1</h3>
             <p class="desc">Escribe aquí el contenido a desarrollar y agrega una imagen adicional si lo necesitas.</p>
           </div>
         </div>
@@ -180,7 +180,7 @@ class Experience extends ContentBase
             <span class="step">2</span>
           </div>
           <div class="txt-container">
-            <p class="title">TITULAR 2</p>
+            <h3 class="title">TITULAR 2</h3>
             <p class="desc">Escribe aquí el contenido a desarrollar y agrega una imagen adicional si lo necesitas.</p>
           </div>
         </div>
@@ -196,7 +196,7 @@ class Experience extends ContentBase
           </div>
 
           <div class="txt-container">
-            <p class="title">TITULAR 3</p>
+            <h3 class="title">TITULAR 3</h3>
             <p class="desc">Escribe aquí el contenido a desarrollar y agrega una imagen adicional si lo necesitas.</p>
           </div>
         </div>
@@ -211,7 +211,7 @@ class Experience extends ContentBase
           </div>
 
           <div class="txt-container">
-            <p class="title">TITULAR 4</p>
+            <h3 class="title">TITULAR 4</h3>
             <p class="desc">Escribe aquí el contenido a desarrollar y agrega una imagen adicional si lo necesitas.</p>
             <div class="municipios">
                 <div class="municipio">
