5f6453affbf5f45eade205c065421f08aefa6804
Jekyll <jekyll@Macbook-Andres.local>
2018-01-31 19:54:32 +0100

    fecha validation

--- modules/system/lang/es/validation.php
+++ modules/system/lang/es/validation.php
@@ -12,7 +12,7 @@ return [
     | such as the size rules. Feel free to tweak each of these messages.
     |
     */
-
+    "after_or_equal"       => "Comprueba las fechas introducidas.",
     "accepted"         => "El campo :attribute debe ser aceptado.",
     "active_url"       => "El campo :attribute no es una URL válida",
     "after"            => "El campo :attribute debe ser una fecha posterior a :date.",
