f92ac3b56dd51966a94a785294379f38b6777ab7
Jekyll <jekyll@Macbook-Andres.local>
2018-02-01 10:53:18 +0100

    3

--- vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/message.blade.php
+++ vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/message.blade.php
@@ -1,5 +1,11 @@
 @component('mail::layout')
-   
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+            {{ config('app.name') }}
+        @endcomponent
+    @endslot
+
     {{-- Body --}}
     {{ $slot }}
 
@@ -12,4 +18,10 @@
         @endslot
     @endisset
 
+    {{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved................
+        @endcomponent
+    @endslot
 @endcomponent
