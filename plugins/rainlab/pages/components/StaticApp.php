c2f89bb4860a8dfca5ded8c6e41bb556928806c3
Jekyll <jekyll@Macbook-Andres.local>
2018-02-07 12:46:23 +0100

    event find api

--- plugins/rainlab/pages/components/StaticApp.php
+++ plugins/rainlab/pages/components/StaticApp.php
@@ -287,6 +287,9 @@ class StaticApp extends ComponentBase
 
     public function eventFind($category, $date_start, $date_end, $location)
     {
+        echo $date_start;
+        
+        dd(1):
         $theme = Theme::getActiveTheme();
         $pages = Page::listInTheme($theme, false);
         $events =  new \Illuminate\Support\Collection($pages);
