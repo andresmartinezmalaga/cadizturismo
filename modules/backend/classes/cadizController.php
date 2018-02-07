d270b84675c300817fe870239b9525563b14df55
Jekyll <jekyll@Macbook-Andres.local>
2018-02-07 11:30:02 +0100

    2

--- modules/backend/classes/cadizController.php
+++ modules/backend/classes/cadizController.php
@@ -4,12 +4,17 @@ use Illuminate\Routing\Controller as ControllerBase;
 use Backend;
 use Backend\Classes\Controller;
 use Illuminate\Http\JsonResponse;
+use Plugins\Rainlab\Pages\Components\StaticApp;
 
 class CadizController extends ControllerBase
 {
+	public function __construct() 
+    {
+    	 $this->StaticApp = new StaticApp; 
+    }
+
 	public function municipiosList()
     {
-    	//
     	return new JsonResponse(['data'=>'HOLA'], 200);
     }
 }
