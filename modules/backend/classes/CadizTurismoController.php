a0f98a17ff6b5f478bfd54c69c3c70002f971f12
Jekyll <jekyll@Macbook-Andres.local>
2018-02-08 16:01:25 +0100

    20

--- modules/backend/classes/CadizTurismoController.php
+++ modules/backend/classes/CadizTurismoController.php
@@ -180,10 +180,9 @@ class CadizTurismoController extends ControllerBase
 
     	//return new JsonResponse(['data'=>$mlPages], 200);
     	
-    	foreach ($mlPages as $iPage) {
-    			echo json_encode($iPage);
-    			dd(1);
-    		if (strpos($iPage[0]['es']['viewBag']['url'], '/naturaleza/espacios-naturales') !== false) {               
+    	foreach ($mlPages as $key => $iPage) {
+    			
+    		if (strpos($iPage->es->viewBag->url, '/naturaleza/espacios-naturales') !== false) {               
                
                 $espacios_naturales->push($iPage);
             } else {
