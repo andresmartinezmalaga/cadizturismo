d5ebe17319e7636ebdf0b9620623eaa4f4c5250d
Jekyll <jekyll@Macbook-Andres.local>
2018-02-08 11:34:33 +0100

    23

--- plugins/rainlab/translate/classes/MLCmsObject.php
+++ plugins/rainlab/translate/classes/MLCmsObject.php
@@ -29,6 +29,9 @@ class MLCmsObject extends CmsCompoundObject
 
     public static function forLocale($locale, $page)
     {
+        echo $page->theme;
+        dd(1);
+        
         static::setContext($locale, $page);
 
         return static::inTheme($page->theme);
