d51061d17cbd4010ca903848e6fdc9baca328798
Jekyll <jekyll@Macbook-Andres.local>
2018-02-08 17:13:16 +0100

    4

--- plugins/rainlab/pages/components/StaticApp.php
+++ plugins/rainlab/pages/components/StaticApp.php
@@ -471,7 +471,7 @@ class StaticApp extends ComponentBase
 
         $daysOperator = '=';
         if($days == 'all'){
-            $days = '2';
+            $days = '%2%';
             $daysOperator = 'LIKE';
         }
 
