96d1f5fb9ce8238065cbfe4d52e8ffb81a4c5228
Jekyll <jekyll@Macbook-Andres.local>
2018-02-08 11:49:15 +0100

    get multil markup

--- plugins/rainlab/translate/classes/MLCmsObject.php
+++ plugins/rainlab/translate/classes/MLCmsObject.php
@@ -29,8 +29,7 @@ class MLCmsObject extends CmsCompoundObject
 
     public static function forLocale($locale, $page)
     {        
-        echo $locale;
-        dd(1);
+        
         static::setContext($locale, $page);
 
         return static::inTheme($page->theme);
