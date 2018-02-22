22bf617f4563caeb183d1a129f728673af3098af
Jekyll <jekyll@Macbook-Andres.local>
2018-02-22 18:02:09 +0100

    5

--- modules/backend/classes/CadizTurismoController.php
+++ modules/backend/classes/CadizTurismoController.php
@@ -283,7 +283,7 @@ class CadizTurismoController extends ControllerBase
         $empresaes =  $this->StaticApp->empresasFindBySlug($nameslug);
         $result->put('empresa-es',$empresa);
         
-        $lang ='en'
+        $lang ='en';
         $empresa[$lang] =  $this->StaticApp->empresasFindBySlug($nameslug);
         $empresa[$lang]->translateContext('en');
         $result->put('empresa-en',$empresa[$lang]);
