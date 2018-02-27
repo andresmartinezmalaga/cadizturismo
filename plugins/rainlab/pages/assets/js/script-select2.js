e2afecd8aa84e671c50148033b2416f1c09d2152
Marina Nieblas <marina.nieblas@hotmail.com>
2018-02-27 16:58:18 +0100

    x

--- plugins/rainlab/pages/assets/js/script-select2.js
+++ plugins/rainlab/pages/assets/js/script-select2.js
@@ -29,6 +29,6 @@ $('#sltLocation').select2({
     if ($('.select2-selection__choice').length == 1) {
       console.log('hola');
       $('#sltLocation').val('todos-los-municipios');
-      return false;
+      //return false;
     }
   });
