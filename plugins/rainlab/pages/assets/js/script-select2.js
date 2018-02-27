8b4c3b4557a5a25005499505c0632e72fd3a7e06
Marina Nieblas <marina.nieblas@hotmail.com>
2018-02-27 17:04:56 +0100

    x

--- plugins/rainlab/pages/assets/js/script-select2.js
+++ plugins/rainlab/pages/assets/js/script-select2.js
@@ -26,7 +26,7 @@ $('#sltLocation').select2({
 
 
   $("#sltLocation").on("select2:unselecting", function(e) {
-    if ($('.select2-selection__choice').length == 1 || $('#sltLocation').val() == 'todos-los-municipios') {
+    if ($('#sltLocation').val() == 'todos-los-municipios') {
       console.log('jeje');
       return false;
     }
