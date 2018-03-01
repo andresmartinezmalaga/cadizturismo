3eefea48ff7ef8393aa44980922d5004b3d069c0
Jekyll <jekyll@Macbook-Andres.local>
2018-03-01 23:09:54 +0100

    ok

--- modules/backend/models/Empresa.php
+++ modules/backend/models/Empresa.php
@@ -164,11 +164,6 @@ class Empresa extends Model
      */
     public function afterCreate()
     {
-        //$this->update(['slug' => str_slug($this->name)]);
-        //$this->update(['mslug' => str_slug($this->municipality)]);
-        /*$this->slug = str_slug($this->name);
-        $this->mslug = str_slug($this->municipality);
-        $this->save();*/
     }
 
 }
