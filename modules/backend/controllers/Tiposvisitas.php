4deb24d95cf7ffdb986c2b2e2a4d02a73a56fb71
Jekyll <jekyll@Macbook-Andres.local>
2018-03-12 15:53:27 +0100

    menu catg

---
+++ modules/backend/controllers/Tiposvisitas.php
@@ -0,0 +1,60 @@
+<?php namespace Backend\Controllers;
+
+use Backend\Classes\Controller;
+
+
+/**
+ * Backend user controller
+ *
+ * @package october\backend
+ * @author Andrés Martínez
+ *
+ */
+class Tiposvisitas extends Controller
+{
+    /**
+     * @var array Extensions implemented by this controller.
+     */
+    public $implement = [
+        \Backend\Behaviors\FormController::class,
+        \Backend\Behaviors\ListController::class
+    ];
+
+    /**
+     * @var array `FormController` configuration.
+     */
+    public $formConfig = 'config_form.yaml';
+
+    /**
+     * @var array `ListController` configuration.
+     */
+    public $listConfig = 'config_list.yaml';
+
+    /**
+     * @var string HTML body tag class
+     */
+    public $bodyClass = 'compact-container';
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {        
+        parent::__construct();
+    }
+
+    /**
+     * Update controller
+     */
+    public function update($recordId, $context = null)
+    {
+        return $this->asExtension('FormController')->update($recordId, $context);
+    }
+
+    
+    /*public function formAfterSave($model)
+    {
+        $model->update(['slug' => str_slug($model->name)]);
+    }*/ 
+
+}
---
+++ modules/backend/models/Tiposvisitas.php
@@ -0,0 +1,75 @@
+<?php namespace Backend\Models;
+
+use Mail;
+use Event;
+use Backend;
+use Model;
+
+/**
+ * Administrator user model
+ *
+ * @package october\backend
+ * @author Andrés Martínez
+ */
+class Tiposvisitas extends Model
+{
+    /**
+     * @var string The database table used by the model.
+     */
+    protected $table = 'tiposvisitas';
+
+    protected $fillable = [];
+
+    public $translatable = [
+        'name'
+    ];
+
+    public static function boot()
+    {
+        // Call default functionality (required)
+        parent::boot();
+
+        // Check the translate plugin is installed
+        if (!class_exists('RainLab\Translate\Behaviors\TranslatableModel'))
+            return;
+
+        // Extend the constructor of the model
+        self::extend(function($model){
+            // Implement the translatable behavior
+            $model->implement[] = 'RainLab.Translate.Behaviors.TranslatableModel';
+
+        });
+    }
+
+    /**
+     * Validation rules
+     */
+    public $rules = [
+    ];
+
+    /**
+     * Relations
+     */
+    public $belongsToMany = [
+    ];
+
+    public $belongsTo = [
+    ];
+
+    public $attachOne = [
+        
+    ];
+
+    
+
+   
+    /**
+     * After create
+     * @return void
+     */
+    public function afterCreate()
+    {
+        
+    }
+
+}
