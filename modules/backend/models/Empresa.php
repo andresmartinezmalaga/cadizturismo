1ae3b563ec50c8c50d09aceecbe5299ff2ab24e8
Jekyll <jekyll@Macbook-Andres.local>
2018-05-15 17:13:05 +0200

    delete 3 last muncp

--- modules/backend/models/Empresa.php
+++ modules/backend/models/Empresa.php
@@ -172,9 +172,7 @@ class Empresa extends Model
         'villaluenga del rosario' => 'Villaluenga del Rosario',
         'villamartín' => 'Villamartín',
         'zahara de la sierra' => 'Zahara de la Sierra',
-        'zahara de los atunes (barbate)' => 'Zahara de los Atunes (Barbate)',
-        'zahara de los atunes (Tarifa)' => 'Zahara de los Atunes (Tarifa)',
-        'caños de meca' => 'Caños de Meca'
+        'zahara de los atunes' => 'Zahara de los Atunes'
         ];
     }
 
--- plugins/rainlab/pages/components/StaticApp.php
+++ plugins/rainlab/pages/components/StaticApp.php
@@ -194,9 +194,7 @@ class StaticApp extends ComponentBase
         'villaluenga del rosario' => 'Villaluenga del Rosario',
         'villamartín' => 'Villamartín',
         'zahara de la sierra' => 'Zahara de la Sierra',
-        'zahara de los atunes (barbate)' => 'Zahara de los Atunes (Barbate)',
-        'zahara de los atunes (Tarifa)' => 'Zahara de los Atunes (Tarifa)',
-        'caños de meca' => 'Caños de Meca'
+        'zahara de los atunes' => 'Zahara de los Atunes'
         ];
     }
 
