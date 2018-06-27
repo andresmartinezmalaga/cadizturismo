0a8ddabc7150b42f91e5227b0f053f77d6aa9252
Marina Nieblas <marina.nieblas@hotmail.com>
2018-06-27 12:20:19 +0200

    prueba

--- plugins/rainlab/pages/controllers/Index.php
+++ plugins/rainlab/pages/controllers/Index.php
@@ -1338,7 +1338,7 @@ class Index extends Controller
         $markup = str_replace("<p><br></p>", "", $markup);
         $markup = str_replace("<p></p>", "", $markup);
         $markup = str_replace("<p>&nbsp;</p>", "", $markup);
-        $markup = str_replace("<br>", "", $markup);
+        //$markup = str_replace("<br>", "", $markup);
         $markup = str_replace("&nbsp;", "", $markup);
 
         return $markup;
